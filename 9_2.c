669e6750bd8895cad416b2d6360474f68f66b663
Md. Iffatul Islam Anon <105257268+iffatul-anon@users.noreply.github.com>
2022-12-09 00:39:48 +0600

    Update 9_2.c

--- 9_2.c
+++ 9_2.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct node
 {
@@ -27,37 +27,39 @@ void inorder(struct node *root)
 
 void preorder(struct node *root)
 {
-    if (root)
-    {
-        printf("%d ->",root->key);
-        preorder(root->left);
-        preorder(root->right);
-    }
-
+	if (root)
+	{
+		printf("%d ->", root->key);
+		preorder(root->left);
+		preorder(root->right);
+	}
 }
 
-void postorder(struct node * root)
+void postorder(struct node *root)
 {
-    if (root)
-    {
-        postorder(root->left);
-        postorder(root->right);
-        printf("%d ->",root->key);
-    }
+	if (root)
+	{
+		postorder(root->left);
+		postorder(root->right);
+		printf("%d ->", root->key);
+	}
 }
 
 struct node *insert(struct node *node, int key)
 {
-	if (node == NULL) {
-        return newNode(key);
-    }
+	if (node == NULL)
+	{
+		return newNode(key);
+	}
 
-	if (key < node->key){
+	if (key < node->key)
+	{
 		node->left = insert(node->left, key);
-    }
-	else if (key > node->key){
+	}
+	else if (key > node->key)
+	{
 		node->right = insert(node->right, key);
-    }
+	}
 	return node;
 }
 
@@ -65,14 +67,14 @@ int main()
 {
 	struct node *root = NULL;
 	root = insert(root, 30);
-	insert(root, 7);
 	insert(root, 15);
-	insert(root, 17);
-	insert(root, 22);
-	insert(root, 27);
-	insert(root, 45);
 	insert(root, 60);
+	insert(root, 7);
+	insert(root, 22);
+	insert(root, 45);
 	insert(root, 75);
+	insert(root, 17);
+	insert(root, 27);
 
 	printf("Pre Order\n");
 	preorder(root);
